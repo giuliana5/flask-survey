0f77d941147262af36a296ef18e64ee9a14523aa
Giuliana <giuliana.torres05@gmail.com>
2022-08-11 13:07:57 -0600

    Added page for each question and function to save answers.

--- app.py
+++ app.py
@@ -14,3 +14,15 @@ def survey_home_page():
   title = satisfaction_survey.title
   instructions = satisfaction_survey.instructions
   return render_template("start.html", title=title, instructions=instructions)
+
+@app.route("/questions/<int:num>")
+def questions(num):
+  question = satisfaction_survey.questions[num].question
+  choices = satisfaction_survey.questions[num].choices
+  return render_template("questions.html", question=question, choices=choices, num=num)
+
+@app.route("/questions/<int:num>", methods=["POST"])
+def add_answer(num):
+  answer = request.form["answer"]
+  responses.append(answer)
+  return redirect(f"/questions/{num + 1}")
