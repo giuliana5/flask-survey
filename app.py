a69f3729b99c316e547ba5476b2f87a4d4baee20
Giuliana <giuliana.torres05@gmail.com>
2022-08-10 18:43:41 -0600

    Added start page to begin the survey. Added templates and function to redirect to questions.

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret-secret"
 
 debug = DebugToolbarExtension(app)
 
+responses = []
+
+@app.route("/")
+def survey_home_page():
+  title = satisfaction_survey.title
+  instructions = satisfaction_survey.instructions
+  return render_template("start.html", title=title, instructions=instructions)
