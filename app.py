3c4ea7e77281ed2ad114be8152c0db4c2ab58b2a
Giuliana <giuliana.torres05@gmail.com>
2022-08-15 04:02:12 -0600

    Updated functions to prevent invalid question urls. Added a function for the comfirmation page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -16,13 +16,25 @@ def survey_home_page():
   return render_template("start.html", title=title, instructions=instructions)
 
 @app.route("/questions/<int:num>")
-def questions(num):
-  question = satisfaction_survey.questions[num].question
-  choices = satisfaction_survey.questions[num].choices
+def ask_question(num):
+  questions = satisfaction_survey.questions
+  if len(responses) == len(questions):
+    return redirect("/finish")
+  if num != len(responses):
+    flash("Invalid question request")
+    return redirect(f"/questions/{len(responses)}")
+  question = questions[num].question
+  choices = questions[num].choices
   return render_template("questions.html", question=question, choices=choices, num=num)
 
 @app.route("/questions/<int:num>", methods=["POST"])
 def add_answer(num):
   answer = request.form["answer"]
   responses.append(answer)
+  if len(responses) == len(satisfaction_survey.questions):
+    return redirect("/finish")
   return redirect(f"/questions/{num + 1}")
+
+@app.route("/finish")
+def finish():
+  return render_template("thank-you.html")
